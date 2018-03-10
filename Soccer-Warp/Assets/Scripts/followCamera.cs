46967a34c3836f3f02225f84350eead3217195f7
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-03-10 13:32:16 -0500

    getting started

---
+++ Soccer-Warp/Assets/Scripts/followCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour {
+
+    //Variables
+    public Transform Ball;
+    public float smooth = 0.3f;
+
+    public float height;
+
+    private Vector3 velocity = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 pos = new Vector3();
+        pos.x = Ball.position.x;
+        pos.z = Ball.position.z;
+        pos.y = Ball.position.y + height;
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+
+	}
+}
